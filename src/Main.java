8f830bc82388d53898cc0354fa067e0acd2a7a1e
AlejandroGasca09 <119777270+AlejandroGasca09@users.noreply.github.com>
2023-03-17 17:31:16 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //PREPARRAMOS PARA LEER LA CLASE SCANNER
+        Scanner teclado = new Scanner(System.in);
+        //EL USUARIO INGRESA SU NOMBRE
+        System.out.println("Ingrese su nombre :");
+        String nombre = teclado.nextLine();
+        //HOLA MUNDO PERZONALIZADO
+        System.out.println("Hola mundo " + nombre);
+        //CERRAMOS LA CLASE SCANNER
+        //Scanner.close();
     }
 }
